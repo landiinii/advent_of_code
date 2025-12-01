50cc0dc85940482e96cfe611912b81afe0679afd
Landen Bailey <landen@getredo.com>
2025-12-01 14:51:14 -0700

    Day 1 part 2

--- 2025/Day1/main.go
+++ 2025/Day1/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	input := readInput("test.txt")
+	input := readInput("input.txt")
 
 	part1(input)
-	// part2(input)
+	part2(input)
 }
 
 func part1(input []string) {
@@ -40,6 +41,31 @@ func part1(input []string) {
 
 func part2(input []string) {
 	fmt.Println("Part 2")
+	start := 50
+	zeros := 0
+
+	for _, line := range input {
+
+		step := get_step(line)
+
+		zeros += int(math.Floor(math.Abs(float64(step) / 100)))
+
+		new_position := start + (step % 100)
+		if new_position < 0 {
+			new_position = 100 + new_position
+			if start != 0 {
+				zeros += 1
+			}
+		} else if new_position > 99 {
+			new_position = new_position - 100
+			zeros += 1
+		} else if new_position == 0 {
+			zeros += 1
+		}
+
+		start = new_position
+	}
+	fmt.Println(zeros)
 }
 
 func get_step(line string) int {
